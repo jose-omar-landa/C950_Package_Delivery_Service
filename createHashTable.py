35a08a9a7eaea5aa4a6f85ccd011f73d485318e9
jose-omar-landa <jlanda4@wgu.edu>
2023-10-28 17:44:41 -0500

    updated comments on hash table

--- createHashTable.py
+++ createHashTable.py
@@ -1,7 +1,4 @@
-
-
 class MyHashTable:
-
     # constructor that sets the array size and initializes the map array
     def __init__(self, initial_size=40):
         self.table = []
@@ -12,29 +9,22 @@ class MyHashTable:
     def insert(self, key, value):
         hash_bucket = hash(key) % len(self.table)
         hash_bucket_list = self.table[hash_bucket]
-
         # this will update the key if it already exists within the hash bucket list
         for key_value in hash_bucket_list:
             if key_value[0] == key:
                 key_value[1] = value
                 return True
-
-        # if the key does not already exist within the hash bucket list, this will insert the
-        # new value into the hash bucket list
+        # if the key does not already exist within hash bucket list, this will insert the new value into the list
         item = [key, value]
         hash_bucket_list.append(item)
         return True
 
     # this function will search for a value with the matching key within the hash table.
-    # if the value is found, the function will return the value.
-    # if the value is not found, the function will return None.
+    # if the value is found, the function will return the value, or will return None if not found.
     def find(self, key):
-
         # this section will find the list where the given key is found
         hash_bucket = hash(key) % len(self.table)
         hash_bucket_list = self.table[hash_bucket]
-
-        # this section will look for the key within the provided list.
         # will return either the corresponding value or None depending on if the key is found or not.
         for key_value_pair in hash_bucket_list:
             if key == key_value_pair[0]:
@@ -42,15 +32,10 @@ class MyHashTable:
         return None
 
     # function to delete an item from the hash table for a given key
-    # will only delete if the given key is found within the hash table
     def delete(self, key):
-
-        # this section will find the list where the given key is found
+        # this section will find the list where the given key is found, and delete the value from the list
         hash_bucket = hash(key) % len(self.table)
         hash_bucket_list = self.table[hash_bucket]
-
-        # this section will delete the value from the list if the key is found
-        # will not remove anything if the key is not found
         if key in hash_bucket_list:
             hash_bucket_list.remove(key)
 
